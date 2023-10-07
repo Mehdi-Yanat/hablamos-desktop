06dd283dc77b2f2a1b0c016e5d9d6dc6f1266119
Yanat Mehdi <78317632+Mehdi-Yanat@users.noreply.github.com>
2023-10-07 12:28:38 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'http://192.168.1.7:4444'
-       baseURL: 'https://aywa.hablamos.website'
+       baseURL: 'https://api.hablamos.website'
     // baseURL: 'https://fierce-inlet-31066.herokuapp.com'
 });
 
